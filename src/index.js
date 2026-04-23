ded71ebf9f0ae5fc7f8392815fc861301dca5984
rvn0816 <102936694+rvn0816@users.noreply.github.com>
2026-04-23 21:53:21 +0800

    brand identity & variables

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ const navButtons = document.querySelectorAll('nav button');
 
 navButtons.forEach(button => {
     button.addEventListener('click', (e) => {
+        // remove .active
+        navButtons.forEach(btn => btn.classList.remove('active'));
+
+        // add .active to the clicked btn
+        e.target.classList.toggle('active');
+
         const pageKey = e.target.dataset.page;
         const pageFunction = pageMapper[pageKey];
 
